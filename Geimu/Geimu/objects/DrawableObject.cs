cbf5dadba8bed3530c2180c673d0ebd7051937ca
Obscuron <Abscuron@gmail.com>
2013-08-05 01:16:52 -0400

    Began work on abstracting game objects

---
+++ Geimu/Geimu/objects/DrawableObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Geimu {
+    
+    // Abstract class for all drawable game objects
+    public abstract class DrawableObject {
+
+        protected static Texture2D sSprite;
+
+        public static virtual String FileName() { return null; }
+
+        public static void LoadContent(ContentManager content) {
+            if (FileName() != null)
+                sSprite = content.Load<Texture2D>(FileName());
+        }
+
+    }
+
+}
