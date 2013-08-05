482eb8b5e6d9f481903902e89e41c6675e06d1cc
Obscuron <Abscuron@gmail.com>
2013-08-05 17:08:20 -0400

    Set up sprite struct

--- Geimu/Geimu/objects/DrawableObject.cs
+++ Geimu/Geimu/objects/DrawableObject.cs
@@ -10,14 +10,29 @@ namespace Geimu {
     
     // Abstract class for all drawable game objects
     public abstract class DrawableObject {
+        // Texture
+        protected static Texture2D sprite;
 
-        protected static Texture2D sSprite;
+        protected List<Sprite> spriteList = new List<Sprite>();
 
-        public static virtual String FileName() { return null; }
+        // Filename - set this
+        public abstract String FileName {
+            get { return ""; }
+        }
 
-        public static void LoadContent(ContentManager content) {
-            if (FileName() != null)
-                sSprite = content.Load<Texture2D>(FileName());
+        // Loads the texture
+        public void LoadContent(ContentManager content) {
+            if (sprite == null)
+                sprite = content.Load<Texture2D>(FileName);
+        }
+
+        // Draws the sprite
+        public virtual void Draw(SpriteBatch spriteBatch) {
+            spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend);
+
+            //spriteBatch.Draw(sSprite, mPos, sector, Color.White, mRot, origin, mScale, SpriteEffects.None, 0);
+
+            spriteBatch.End();
         }
 
     }
---
+++ Geimu/Geimu/objects/Sprite.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Geimu {
+
+    // Struct representing a single sprite
+    public struct Sprite {
+        // Top left corner
+        private Vector2 pos;
+        private Vector2 origin;
+        private Rectangle sector;
+        private float width;
+        private float height;
+
+        // Properties
+        public Vector2 Pos {
+            get { return pos + origin * Scale; }
+            set { pos = value - origin * Scale; }
+        }
+        public Vector2 Origin {
+            get { return origin; }
+            set {
+                pos += (value - origin) * Scale;
+                origin = value;
+            }
+        }
+        public Rectangle Sector {
+            get { return sector; }
+            set {
+                sector = value;
+                width = sector.Width;
+                height = sector.Height;
+            }
+        }
+        public float Scale {
+            get;
+            set;
+        }
+        public Color Tint {
+            get;
+            set;
+        }
+
+        public float Width {
+            get { return width * Scale; }
+            set { width = value / Scale; }
+        }
+        public float Height {
+            get { return height * Scale; }
+            set { height = value / Scale; }
+        }
+
+        public float Top {
+            get { return pos.Y; }
+            set { pos.Y = value; }
+        }
+        public float Bot {
+            get { return pos.Y + Height; }
+            set { pos.Y = value - Height; }
+        }
+        public float Left {
+            get { return pos.X; }
+            set { pos.X = value; }
+        }
+        public float Right {
+            get { return pos.X + Width; }
+            set { pos.X = value - Width; }
+        }
+
+        public Sprite(Rectangle sector) : this() {
+            Scale = 1.0f;
+            Tint = Color.White;
+
+            Sector = sector;
+        }
+
+    }
+
+}
